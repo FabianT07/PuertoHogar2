2587fc3e9cf295ef5495de8eb33f48738070af25
FabianT07 <145619517+FabianT07@users.noreply.github.com>
2026-07-18 21:15:56 -0500

    Delete app/actions/page.tsx

--- app/actions/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getProperties } from "@/app/actions/properties"
-import { HomeClient } from "@/components/home-client"
-
-export default async function Page() {
-  const properties = await getProperties()
-  return <HomeClient initialProperties={properties} />
-}
